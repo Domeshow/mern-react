a6cd6686661905563c915db6c55388b0196c63b9
Domeshow <domeshowemmanuel@gmail.com>
2022-05-17 23:18:44 +0200

    Accepting body data

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 
 const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 const port = process.env.PORT || 4000;
 
 app.use('/api/goals', require("./routes/goalRoutes"))
